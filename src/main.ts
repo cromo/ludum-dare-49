b722b7c92c3789dacecf4737097d35189c514e7e
Zachery Delafosse <natsogames@yahoo.com>
2021-10-03 16:26:57 -0500

    oob tile and level templates

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { drawLevel, drawLevelEntities, initBackgroundCanvas, loadLevel, reloadLe
 import { initFastRandom } from "./glitch";
 import * as input from "./input";
 import { getCurrentLevel } from "./levels";
+import { blankCorridorLevel } from "./levels/blankCorridorLevel";
 import { debugLevel } from "./levels/debugLevel";
 import { sampleLevelEmpty } from "./levels/sampleLevel";
+import { simpleWalkLevel } from "./levels/simpleWalkLevel";
 import { GAME_SCALE, buildStructresAtInit } from "./models";
 import { loadPlayerSprites } from "./player";
 
@@ -11,7 +13,7 @@ if (os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") === "1") {
   require("@NoResolution:lldebugger").start();
 }
 
-const LEVEL_SEQUENCE = [debugLevel, sampleLevelEmpty];
+const LEVEL_SEQUENCE = [simpleWalkLevel, blankCorridorLevel, debugLevel, sampleLevelEmpty];
 let currentLevelIndex = 0;
 
 love.load = () => {
