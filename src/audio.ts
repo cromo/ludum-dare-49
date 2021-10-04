52fded551e3f5ed4df2cd32ba3d643b7200ccc13
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-10-04 14:27:25 -0400

    Terminal is now less loud

--- src/audio.ts
+++ src/audio.ts
@@ -46,7 +46,7 @@ export function initAudio(): void {
   availableSfx["exit"] = { source: audio.newSource("res/sfx/tepelort.wav", "static") };
   availableSfx["geiger"] = { source: audio.newSource("res/sfx/geiger-raw.wav", "static"), pitchVariation: 0.2 };
 
-  availableSfx["terminal"] = { source: audio.newSource("res/sfx/typing_dot_matrix.wav", "static") };
+  availableSfx["terminal"] = { source: audio.newSource("res/sfx/typing_dot_matrix.wav", "static"), volume: 0.5 };
   availableSfx["charge"] = {
     source: audio.newSource("res/sfx/dash_charge.wav", "static"),
     pitchVariation: 0.1,
